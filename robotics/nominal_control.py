ab4405bc05d6c3123d710e949ddfca8f0235c092
Berk Tosun <tb.tosunberk@gmail.com>
2022-07-09 23:51:02 +0300

    [not working] Add nominal control

--- robotics/nominal_control.py
+++ robotics/nominal_control.py
@@ -1,9 +1,11 @@
 """
-From ilqr/examples/cartpole.ipynb
+Copied from ilqr/examples/cartpole.ipynb
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.animation as animation
 
 from ilqr import iLQR
 from ilqr.cost import QRCost
@@ -21,3 +23,114 @@ pole_length = 1.0
 m_cart = 1.0
 m_pole = 0.1
 dynamics = CartpoleDynamics(dt, l=pole_length)
+
+# Note that the augmented state is not all 0.
+x_goal = dynamics.augment_state(np.array([0.0, 0.0, 0.0, 0.0]))
+
+# Instantenous state cost.
+Q = np.eye(dynamics.state_size)
+Q[0, 0] = 1.0
+Q[1, 1] = Q[4, 4] = 0.0
+Q[0, 2] = Q[2, 0] = pole_length
+Q[2, 2] = Q[3, 3] = pole_length**2
+R = 0.1 * np.eye(dynamics.action_size)
+
+# Terminal state cost.
+Q_terminal = 100 * np.eye(dynamics.state_size)
+
+# Instantaneous control cost.
+R = np.array([[0.001]])
+
+cost = QRCost(Q, R, Q_terminal=Q_terminal, x_goal=x_goal)
+
+N = 500
+x0 = dynamics.augment_state(np.array([0.0, 0.0, 0.2, 0.0]))
+us_init = np.random.uniform(-1, 1, (N, dynamics.action_size))
+ilqr = iLQR(dynamics, cost, N)
+
+
+J_hist = []
+xs, us = ilqr.fit(x0, us_init, n_iterations=500, on_iteration=on_iteration)
+
+def visualize(l, y, t, dt, save=None):
+    """
+    Args:
+        l: Pendulum length, CartPole.l
+        y: simulation outputs
+        t: simulation timesteps
+        dt
+    """
+    a_x1 = y[:, 0]
+    a_y1 = 0.0
+
+    a_x2 = l * np.sin(y[:, 2]) + a_x1
+    a_y2 = -l * np.cos(y[:, 2]) + a_y1
+
+    fig = plt.figure()
+    ax = fig.add_subplot(221, autoscale_on=True, aspect='equal',\
+                            xlim=(-3, 3), ylim=(-3, 3))
+
+    ax.grid()
+
+    line, = ax.plot([], [], 'o-', lw=2)
+    time_template = 'time = %.1fs'
+    time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+
+    def init():
+        line.set_data([], [])
+        time_text.set_text('')
+        return line, time_text
+
+    def animate(i):
+        thisx = [a_x1[i], a_x2[i]]
+        thisy = [a_y1, a_y2[i]]
+
+        line.set_data(thisx, thisy)
+        time_text.set_text(time_template%(i*dt))
+        return line, time_text
+
+    ani = animation.FuncAnimation(fig, animate, np.arange(1, len(y)),
+        interval=30, blit=True, init_func=init)
+
+    # time domain plot
+    ax = fig.add_subplot(2, 2, 2)
+    ax.set_xlabel('t')
+    ax.set_ylabel('x')
+    ax.grid()
+    ax.plot(t, y[:, 0])
+    ax.plot(t, y[:, 2])
+
+    ax2 = fig.add_subplot(2, 2, 3)
+    ax2_plt = ax2.scatter(y[:, 0], y[:, 1], c=t, alpha=0.2)
+    ax2.set_title("States (Phase plane)")
+    ax2.set_xlabel("Cart position")
+    ax2.set_ylabel("Cart velocity")
+    ax2.grid(True)
+    ax2.axhline(color='black')
+    ax2.axvline(3.14, color='black')
+    divider = make_axes_locatable(ax2)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    cbar = fig.colorbar(ax2_plt, cax=cax, orientation='vertical')
+    # cbar.set_label('Time')
+
+    ax3 = fig.add_subplot(2, 2, 4)
+    ax3_plt = ax3.scatter(y[:, 2], y[:, 3], c=t, alpha=0.2)
+    ax3.set_title("States (Phase plane)")
+    ax3.set_xlabel("Pitch")
+    ax3.set_ylabel("Pitch dot")
+    ax3.grid(True)
+    ax3.axhline(color='black')
+    ax3.axvline(3.14, color='black')
+    divider = make_axes_locatable(ax3)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    cbar = fig.colorbar(ax3_plt, cax=cax, orientation='vertical')
+    # cbar.set_label('Time')
+
+    if save:
+        ani.save(f'{save}.mp4', fps=20)
+
+    plt.tight_layout()
+    plt.show()
+
+
+visualize(pole_length, xs, np.arange(0, len(xs)*dt, dt), dt)
